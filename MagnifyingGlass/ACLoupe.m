1eb7137be95f00dba8f14201704356108c248fdd
Ben Hirashima <ben@realitycap.com>
2014-01-24 14:22:41 -0800

    Changed loupe bitmap to one extracted from iOS7.

--- MagnifyingGlass/ACLoupe.m
+++ MagnifyingGlass/ACLoupe.m
@@ -23,7 +23,7 @@ static CGFloat const kACLoupeDefaultRadius = 64;
 	if (self = [super initWithFrame:frame]) {
 		self.layer.borderWidth = 0;
 		
-		UIImageView *loupeImageView = [[UIImageView alloc] initWithFrame:CGRectOffset(CGRectInset(self.bounds, -5.0, -5.0), 0, 2)];
+		UIImageView *loupeImageView = [[UIImageView alloc] initWithFrame:CGRectOffset(CGRectInset(self.bounds, -3.0, -3.0), 0, 2.5)];
 		loupeImageView.image = [UIImage imageNamed:@"kb-loupe-hi"];
 		loupeImageView.backgroundColor = [UIColor clearColor];
 		[self addSubview:loupeImageView];
